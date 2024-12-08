ac678a835b27c1593f6d772ea3f2005ff552d703
Andre <andreafragnelli@gmail.com>
2024-12-08 17:26:21 +0100

    feat: Add cors to make API calls possible

--- backend-nodejs/src/app.ts
+++ backend-nodejs/src/app.ts
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import mintRoutes from "./routes/mint";
 import { startListening } from "./listeners/events";
 
 dotenv.config();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
