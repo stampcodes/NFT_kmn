b2bae729de192c3bdae91107d602cb6a90d07460
Andre <andreafragnelli@gmail.com>
2024-12-08 17:35:07 +0100

    feat: integrate wallet connection and NFT minting functionality in App component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,26 @@
+import { useAccount } from "wagmi";
+import MintNFTButton from "./components/MintNFTButton";
+import Navbar from "./components/Navbar";
+import NFTList from "./components/NFTList";
+import MintNFTExc from "./components/MintNFTExc";
+
 function App() {
+  const { address } = useAccount();
+
   return (
     <>
-      <div></div>
+      <div>
+        <Navbar />
+        <MintNFTButton />
+        {address ? (
+          <NFTList ownerAddress={address} />
+        ) : (
+          <p className="text-center mt-10">
+            Connect with your wallet to see your NFTs.
+          </p>
+        )}
+        <MintNFTExc />
+      </div>
     </>
   );
 }
